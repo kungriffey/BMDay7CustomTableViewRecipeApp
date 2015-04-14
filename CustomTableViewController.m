133a7898db1acb462318a7a6059f614fc930ff91
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-14 15:14:56 -0400

    updated to show images for each cell

--- CustomTableViewController.m
+++ CustomTableViewController.m
@@ -16,13 +16,15 @@
 {
   
   NSArray* recipeNames;
-  
+  NSArray* recipeImages;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
   
     recipeNames = @[@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini"];
+  
+  recipeImages = @[@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg",@"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg"];
   
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -56,7 +58,7 @@
   UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
   
   cell.textLabel.text = [recipeNames objectAtIndex:indexPath.row];
-  cell.imageView.image = [UIImage imageNamed:@"creme_brelee"];
+  cell.imageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
   
   return cell;
 }
