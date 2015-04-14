caeabeeb8b4b4888b9f471b4e6f30ce8a96d818d
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-14 14:43:50 -0400

    created a new table view controller

--- CustomTableViewController.h
+++ CustomTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CustomTableViewController.h
 //  BMDaySevenCustomTableAssignment
 //
 //  Created by Kunwardeep Gill on 2015-04-14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CustomTableViewController : UITableViewController
 
 @end
-
