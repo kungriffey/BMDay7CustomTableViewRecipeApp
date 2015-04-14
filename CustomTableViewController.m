2099fdd1c041108dbbffb5b3bdc06a5224c1d446
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-14 14:56:59 -0400

    updated numberOfRows and cellForRowAtIndexPath methods

--- CustomTableViewController.m
+++ CustomTableViewController.m
@@ -13,10 +13,17 @@
 @end
 
 @implementation CustomTableViewController
+{
+  
+  NSArray* recipeNames;
+  
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+  
+    recipeNames = @[@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini"];
+  
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -31,10 +38,25 @@
 
 #pragma mark - Table view data source
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+  
+  return [recipeNames count];
+  
+}
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
     return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+  
+  static NSString *cellIdentifier = @"Cell";
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+  
+  cell.textLabel.text = [recipeNames objectAtIndex:indexPath.row];
+  cell.imageView.image = [UIImage imageNamed:@"creme_brelee";]
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
