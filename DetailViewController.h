e2bcae64408e362b20e0f0f4226a16ed38b293f2
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-16 17:19:36 -0400

    created push segue for the detail view controller

---
+++ DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  BMDaySevenCustomTableAssignment
+//
+//  Created by Kunwardeep Gill on 2015-04-16.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *recipeDetail;
+@property (strong, nonatomic) NSString *recipeName;
+
+@end
