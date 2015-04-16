87a547eba7a8f4149b62b5f34885e34350ef4822
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-16 15:23:40 -0400

    added images to launch screen

--- CustomTableViewController.m
+++ CustomTableViewController.m
@@ -25,11 +25,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
   
-    recipeNames = [NSMutableArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+//    recipeNames = [NSMutableArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+//  
+//    recipeImages = [NSMutableArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg",@"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
+//  
+//    recipePrepTime = [NSMutableArray arrayWithObjects:@"30 minutes", @"40 minutes", @"30 minutes", @"15 minutes", @"20 minutes", @"30 minutes", @"25 minutes", @"10 minutes", @"30 minutes", @"60 minutes", @"40 minutes",@"60 minutes", @"25 minutes", @"30 minutes", @"30 minutes", @"15 minutes", nil];
   
-    recipeImages = [NSMutableArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg",@"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
+  //Using Plist
+  //Retrieve the plist path
+  NSString *path = [[NSBundle mainBundle]pathForResource:@"recipes" ofType:@"plist"];
+  //create a dictionary that connects to the file we made
+  NSMutableDictionary *dict = [[NSMutableDictionary alloc]initWithContentsOfFile:path];
   
-    recipePrepTime = [NSMutableArray arrayWithObjects:@"30 minutes", @"40 minutes", @"30 minutes", @"15 minutes", @"20 minutes", @"30 minutes", @"25 minutes", @"10 minutes", @"30 minutes", @"60 minutes", @"40 minutes",@"60 minutes", @"25 minutes", @"30 minutes", @"30 minutes", @"15 minutes", nil];
+  //specify key value pairs for the dictionary and the arrays in the plist
+  recipeNames = [dict objectForKey:@"Name"];
+  recipeImages = [dict objectForKey:@"Image"];
+  recipePrepTime = [dict objectForKey:@"PrepTime"];
+  
   
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
